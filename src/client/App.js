6dd2bb2a45274d9381b6630556b93022407e890a
Alfredo Yebra <fred.yebra@gmail.com>
2018-07-12 19:15:21 -0500

    Setup front end

---
+++ src/client/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const App = () => {
+	return (
+		<div>
+			<h1>hi world</h1>
+		</div>
+	);
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							minimize: true
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Surveylet',
+			template: './public/index.html'
+		})
+	]
+};
